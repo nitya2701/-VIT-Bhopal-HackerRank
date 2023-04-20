e0d791d53ff5ce037be6629348cd0e1a04ac5799
Nitya Bansal <80637539+nitya2701@users.noreply.github.com>
2023-04-20 22:15:04 +0530

    Update Minimum Moves to Equal Array Elements.java

--- Minimum Moves to Equal Array Elements.java
+++ Minimum Moves to Equal Array Elements.java
@@ -1,3 +1,4 @@
+/*
 Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
 In one move, you can increment or decrement an element of the array by 1.
 Test cases are designed so that the answer will fit in a 32-bit integer.
@@ -32,7 +33,7 @@ Sample Input 1
 Sample Output 1
 
 16
-/*
+*/
 
 import java.io.*;
 import java.util.*;
