2c9eb21c6631d87b990fdb896e71ed9cea84f85f
Nitya Bansal <80637539+nitya2701@users.noreply.github.com>
2023-10-28 11:35:30 +0530

    Time: 39 ms (66.43%), Space: 15.8 MB (99.98%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.res=[]
+
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        arr=''
+        if not root:
+            return []
+        self.checkSm(root, arr)
+        return self.res
+    
+    def checkSm(self, node, arr):
+        newarr=arr+str(node.val)
+        if not node.left and not node.right:
+            self.res.append(newarr)
+            return
+        else:
+            newarr=newarr+'->'
+            if node.left:
+                self.checkSm(node.left, newarr)
+            if node.right:
+                self.checkSm(node.right, newarr)
+        return
